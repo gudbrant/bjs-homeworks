5bc6d120f8907e23abf738fcf6cb85770e417c5a
Anton Isaev <gudbrant@yandex.ru>
2020-08-01 22:31:58 +0800

    Update task.js

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -5,23 +5,22 @@
 	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
  */
 
-function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
-}
+ function averageMark(algebra, geography, physics) {
+    let sumMark = (algebra + geography + physics) / 3;
+    return sumMark;
+ }
 
-function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
-}
+ function sayHello(userName) {
+    let myName = userName;
+    let message = 'Привет, мир! Меня зовут ' + myName;
+    return message;
+ }
 
-function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
-}
+ function calculateFormula() {
+    let x = 2;
+    let y = 22;
+    let z = 0;
+    let solution = (x * y) + (5 * z) + (x -1);
+    let result = solution
+    return result;
+ }
