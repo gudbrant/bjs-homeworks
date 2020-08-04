d8f90953f51aad7a41f0c78dfefe57d6e68fa00c
gudbrant <61345734+gudbrant@users.noreply.github.com>
2020-08-04 10:29:39 +0800

    Update task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,5 +1,24 @@
 `use String`
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    
+    if (percent > 0) {  
+        percent = +percent;
+    } else if (percent != 0 || percent == 0) {
+          return (`“Параметр "Процентная ставка" содержит неправильное значение ${percent}”.`);
+    }
+ 
+    if (contribution >= 0) {  
+        contribution = +contribution;
+    } else if (contribution !=  0) {
+          return (`“Параметр "Начальный взнос" содержит неправильное значение ${contribution}”.`); 
+    }
+   
+    if (amount >= 0) {  
+        amount = +amount;
+    } else if (amount !=  0) {
+          return (`“Параметр "Общая стоимость" содержит неправильное значение ${amount}”.`); 
+    }
+    
     let currentDate  = new Date();  
     date = new Date(window.date.value); 
     let payPeriod = 0 - ((currentDate.getFullYear() -  date.getFullYear()) * 12) - (currentDate.getMonth() - date.getMonth());
