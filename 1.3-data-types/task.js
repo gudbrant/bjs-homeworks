38f7d5aa0489e9dd67279f8001ea362182cb2886
gudbrant <61345734+gudbrant@users.noreply.github.com>
2020-08-04 10:26:07 +0800

    Update task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,27 @@
+`use String`
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let currentDate  = new Date();  
+    date = new Date(window.date.value); 
+    let payPeriod = 0 - ((currentDate.getFullYear() -  date.getFullYear()) * 12) - (currentDate.getMonth() - date.getMonth());
+    date = payPeriod; 
+    let returnAmount = amount - contribution  
+    percent = percent / 1200; 
+    let monthlyPay = amount*(percent+percent/(((1+percent)**date)-1)); 
+    let  totalAmount = monthlyPay * date;  
+    console.log(totalAmount.toFixed(2));
+    return totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting = '';
+    let RuString = /[^а-яА-ЯёЁ\-\s]/;
+    if (RuString.test(name) || name === "" || name === String(undefined) || name === String(null)) {
+        greeting = `Привет, мир! Меня зовут Аноним.`;
+        console.log(greeting);
+        return greeting;
+    } else  {
+        greeting = `Привет, мир! Меня зовут ${name}.`;
+        console.log(greeting);
+        return greeting; 
+    } 
 }
