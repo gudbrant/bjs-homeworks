18b6d453c7d31f2cedbe903915a72c064f454914
gudbrant <61345734+gudbrant@users.noreply.github.com>
2020-08-12 13:19:56 +0800

    Update task.js

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,41 @@
 //String.prototype.isPalindrome - для задачи №1
 
+
+
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
     // return averageMark
+
+//Задание №2
+    
+function getAverageMark(marks) {
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        marks[i] = Number(marks[i]);
+        sum += marks[i];
+    }
+    
+    let average = sum / marks.length;
+    let roundedAverage = Math.round(average);
+    
+    return roundedAverage;
+}
+
+//Задание №3    
+
+function initCheckBirthday() {
+    let birthday = document.getElementById('birthday').value;
+
+    let result = checkBirthday(birthday) ? "Да" : "Нет";
+
+    document.getElementById('disclaimer').innerHTML = result;   
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now =+ new Date();
+    birthday =+ new Date(birthday);
+    let diff = now - birthday
+    let millisecondsYear = 24 * 60 * 60 * 1000 * 365;
+    let age = diff / millisecondsYear;
+    return (age > 18);
 }
