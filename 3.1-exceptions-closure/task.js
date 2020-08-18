57a12ba2e427cf7eb66b4567a774e8c998d84173
Anton Isaev <gudbrant@yandex.ru>
2020-08-18 19:53:02 +0800

    Update task.js

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -1,11 +1,30 @@
 function parseCount (count) {
-    let newCount = parseInt(count);
-    if (isNaN (count)) {
-        return Err("Невалидное значение");
+    this.count = count;
+    let newCount = parseInt (count);
+    if (isNaN(newCount)) {
+        throw 'Невалидное значение';
     } else {
-        return count;
+        return newCount;
+    }
+    
+}
+
+function validateCount (count) {
+    try {
+        const parsed = parseInt(count);
+        return parsed;
+    } catch {
+        
     }
 }
 
 
 
+class Triangle {
+    constructor (a,b,c) {
+        if (a == b < с || c == a < b || c == a < b) {
+            throw 'Треугольник с такими сторонами не существует';
+        }
+        
+    }
+}
